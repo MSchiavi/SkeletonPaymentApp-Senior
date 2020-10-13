d85dfc93c4ca524a900d3d49d2de3394a3d0c35a
MSchiavi <mmschiav@buffalo.edu>
2020-10-13 19:23:00 -0400

    Using data from TestDatabase to populate products and orderhistory

--- Android/app/src/main/java/com/imobile3/groovypayments/data/GroovyDemoManager.java
+++ Android/app/src/main/java/com/imobile3/groovypayments/data/GroovyDemoManager.java
@@ -2,28 +2,19 @@ package com.imobile3.groovypayments.data;
 
 import android.content.Context;
 import android.os.AsyncTask;
-import android.os.Looper;
 
 import com.imobile3.groovypayments.MainApplication;
+import com.imobile3.groovypayments.calculation.CartCalculator;
 import com.imobile3.groovypayments.data.entities.CartEntity;
 import com.imobile3.groovypayments.data.entities.CartProductEntity;
+import com.imobile3.groovypayments.data.entities.CartTaxEntity;
 import com.imobile3.groovypayments.data.entities.ProductEntity;
 import com.imobile3.groovypayments.data.entities.ProductTaxJunctionEntity;
 import com.imobile3.groovypayments.data.entities.TaxEntity;
-import com.imobile3.groovypayments.data.enums.GroovyColor;
-import com.imobile3.groovypayments.data.enums.GroovyIcon;
-import com.imobile3.groovypayments.data.utils.CartBuilder;
-import com.imobile3.groovypayments.data.utils.CartProductBuilder;
-import com.imobile3.groovypayments.data.utils.ProductBuilder;
-import com.imobile3.groovypayments.data.utils.ProductTaxJunctionBuilder;
-import com.imobile3.groovypayments.data.utils.TaxBuilder;
+import com.imobile3.groovypayments.data.model.Cart;
 
 import androidx.annotation.NonNull;
-import androidx.room.Database;
-
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 public final class GroovyDemoManager {
@@ -104,52 +95,16 @@ public final class GroovyDemoManager {
         @Override
         public void run() {
 
-            List<ProductEntity> productEntities = new ArrayList<>();
+            // Found testData in testRepository
+            List<ProductEntity> productEntities = TestDataRepository.getInstance().getProducts(TestDataRepository.Environment.GroovyDemo);
 
-            List<TaxEntity> taxEntities = new ArrayList<>();
+            List<TaxEntity> taxEntities = TestDataRepository.getInstance().getTaxes(TestDataRepository.Environment.GroovyDemo);
 
             List<ProductTaxJunctionEntity> productTaxJunctionEntities = new ArrayList<>();
 
-            productEntities.add(ProductBuilder.build(101L,
-                    "Tasty Chicken Sandwich",
-                    "Chicken, lettuce, tomato and mayo",
-                    750L, 200L,
-                    GroovyIcon.Sandwich, GroovyColor.Yellow));
-
-            productEntities.add(ProductBuilder.build(102L,
-                    "Delicous Spicy Wings",
-                    "Chicken Wings, Cajun, Blue Cheese",
-                    600L, 100L,
-                    GroovyIcon.Sandwich, GroovyColor.Orange));
-
-            productEntities.add(ProductBuilder.build(103L,
-                    "Tasty Roast Beef Sandwich",
-                    "Beef, horseradish, Salty roll",
-                    750L, 200L,
-                    GroovyIcon.Sandwich, GroovyColor.Yellow));
-
-            productEntities.add(ProductBuilder.build(104L,
-                    "Coffee",
-                    "Hot Coffee",
-                    100L, 100L,
-                    GroovyIcon.CoffeeMug, GroovyColor.Gray));
-
-            taxEntities.add(TaxBuilder.build(101L,
-                    "Chicken", "0.08"));
-            taxEntities.add(TaxBuilder.build(102L,
-                    "Beef", "0.09"));
-            taxEntities.add(TaxBuilder.build(103L,
-                    "Drink", "0.06"));
-
-            //Chicken Sandwich with Chicken tax
-            productTaxJunctionEntities.add(ProductTaxJunctionBuilder.build(101L, 101L));
-            // Chicken wings with Chicken tax
-            productTaxJunctionEntities.add(ProductTaxJunctionBuilder.build(102L, 101L));
-            // Beef Sandwich with Beef tax
-            productTaxJunctionEntities.add(ProductTaxJunctionBuilder.build(103L, 102L));
-            // Coffee with Drink Tax
-            productTaxJunctionEntities.add(ProductTaxJunctionBuilder.build(104, 103));
-
+            for (int i = 0; i < productEntities.size(); i++) {
+                productTaxJunctionEntities.addAll(TestDataRepository.getInstance().getProductTaxJunctions(productEntities.get(i), taxEntities));
+            }
             // Insert entities into database instance.
             DatabaseHelper.getInstance().getDatabase().getProductDao()
                     .insertProducts(
@@ -168,20 +123,38 @@ public final class GroovyDemoManager {
 
         @Override
         public void run() {
-            List<CartEntity> cartEntities = new ArrayList<>();
+            List<CartEntity> cartEntities = TestDataRepository.getInstance().getCarts(TestDataRepository.Environment.GroovyDemo);
+            List<Cart> cartResults = new ArrayList<>();
 
             List<CartProductEntity> cartProductEntities = new ArrayList<>();
+            List<CartTaxEntity> cartTaxEntities = new ArrayList<>();
 
-            //TODO When restarting the app this database entry is not being pulled with a description
+            for(int i = 0; i < cartEntities.size(); i++){
+                Cart cart = new Cart(cartEntities.get(i));
+                cart.setProducts(TestDataRepository.getInstance().getCartProducts(TestDataRepository.Environment.GroovyDemo,cartEntities.get(i)));
+                cart.setTaxes(TestDataRepository.getInstance().getCartTaxes(TestDataRepository.Environment.GroovyDemo,cartEntities.get(i)));
 
-            cartEntities.add(CartBuilder.build(101L, new Date(),1000L,200L,1653L));
-            cartProductEntities.add(CartProductBuilder.build(101L,101L,"Coffee, Delicous Spicy Wings, Tasty Chicken Sandwich",1653L,1));
+
+                new CartCalculator(cart).calculate();
+                cartResults.add(cart);
+
+
+                cartProductEntities.addAll(cart.getProducts());
+                cartTaxEntities.addAll(cart.getTaxes());
+            }
+
+            //Need to remove the old CartEntities and replace with the totals calculated.
+            cartEntities.clear();
+            cartEntities.addAll(cartResults);
 
             DatabaseHelper.getInstance().getDatabase().getCartDao().insertCarts(
                     cartEntities.toArray(new CartEntity[0]));
 
             DatabaseHelper.getInstance().getDatabase().getCartProductDao().insertCartProducts(
                     cartProductEntities.toArray(new CartProductEntity[0]));
+
+            DatabaseHelper.getInstance().getDatabase().getCartTaxDao().insertCartTaxes(
+                    cartTaxEntities.toArray(new CartTaxEntity[0]));
 
         }
     }
