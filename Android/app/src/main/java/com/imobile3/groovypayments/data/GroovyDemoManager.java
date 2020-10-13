ce3438f2b978116e4df9f36d5514e256a24af4e3
MSchiavi <mmschiav@buffalo.edu>
2020-10-13 14:20:19 -0400

    Added Inventory and Cart workers to provide demo when db is reset, created ProductTaxJunctionBuilder class.

--- Android/app/src/main/java/com/imobile3/groovypayments/data/GroovyDemoManager.java
+++ Android/app/src/main/java/com/imobile3/groovypayments/data/GroovyDemoManager.java
@@ -2,16 +2,28 @@ package com.imobile3.groovypayments.data;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Looper;
 
 import com.imobile3.groovypayments.MainApplication;
+import com.imobile3.groovypayments.data.entities.CartEntity;
+import com.imobile3.groovypayments.data.entities.CartProductEntity;
 import com.imobile3.groovypayments.data.entities.ProductEntity;
+import com.imobile3.groovypayments.data.entities.ProductTaxJunctionEntity;
+import com.imobile3.groovypayments.data.entities.TaxEntity;
 import com.imobile3.groovypayments.data.enums.GroovyColor;
 import com.imobile3.groovypayments.data.enums.GroovyIcon;
+import com.imobile3.groovypayments.data.utils.CartBuilder;
+import com.imobile3.groovypayments.data.utils.CartProductBuilder;
 import com.imobile3.groovypayments.data.utils.ProductBuilder;
+import com.imobile3.groovypayments.data.utils.ProductTaxJunctionBuilder;
+import com.imobile3.groovypayments.data.utils.TaxBuilder;
 
 import androidx.annotation.NonNull;
+import androidx.room.Database;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public final class GroovyDemoManager {
@@ -72,19 +84,9 @@ public final class GroovyDemoManager {
             // Initialize a new database instance.
             DatabaseHelper.getInstance().init(mContext);
 
-            List<ProductEntity> productEntities = new ArrayList<>();
+            new InventoryWorker().run();
 
-            // Add one product.
-            productEntities.add(ProductBuilder.build(101L,
-                    "Tasty Chicken Sandwich",
-                    "Chicken, lettuce, tomato and mayo",
-                    750L, 200L,
-                    GroovyIcon.Sandwich, GroovyColor.Yellow));
-
-            // Insert entities into database instance.
-            DatabaseHelper.getInstance().getDatabase().getProductDao()
-                    .insertProducts(
-                            productEntities.toArray(new ProductEntity[0]));
+            new OrderHistoryWorker().run();
 
             // All done!
             return null;
@@ -94,6 +96,92 @@ public final class GroovyDemoManager {
         protected void onPostExecute(Void result) {
             super.onPostExecute(result);
             mCallback.onDatabaseReset();
+        }
+    }
+
+    private class InventoryWorker implements Runnable {
+
+        @Override
+        public void run() {
+
+            List<ProductEntity> productEntities = new ArrayList<>();
+
+            List<TaxEntity> taxEntities = new ArrayList<>();
+
+            List<ProductTaxJunctionEntity> productTaxJunctionEntities = new ArrayList<>();
+
+            productEntities.add(ProductBuilder.build(101L,
+                    "Tasty Chicken Sandwich",
+                    "Chicken, lettuce, tomato and mayo",
+                    750L, 200L,
+                    GroovyIcon.Sandwich, GroovyColor.Yellow));
+
+            productEntities.add(ProductBuilder.build(102L,
+                    "Delicous Spicy Wings",
+                    "Chicken Wings, Cajun, Blue Cheese",
+                    600L, 100L,
+                    GroovyIcon.Sandwich, GroovyColor.Orange));
+
+            productEntities.add(ProductBuilder.build(103L,
+                    "Tasty Roast Beef Sandwich",
+                    "Beef, horseradish, Salty roll",
+                    750L, 200L,
+                    GroovyIcon.Sandwich, GroovyColor.Yellow));
+
+            productEntities.add(ProductBuilder.build(104L,
+                    "Coffee",
+                    "Hot Coffee",
+                    100L, 100L,
+                    GroovyIcon.CoffeeMug, GroovyColor.Gray));
+
+            taxEntities.add(TaxBuilder.build(101L,
+                    "Chicken", "0.08"));
+            taxEntities.add(TaxBuilder.build(102L,
+                    "Beef", "0.09"));
+            taxEntities.add(TaxBuilder.build(103L,
+                    "Drink", "0.06"));
+
+            //Chicken Sandwich with Chicken tax
+            productTaxJunctionEntities.add(ProductTaxJunctionBuilder.build(101L, 101L));
+            // Chicken wings with Chicken tax
+            productTaxJunctionEntities.add(ProductTaxJunctionBuilder.build(102L, 101L));
+            // Beef Sandwich with Beef tax
+            productTaxJunctionEntities.add(ProductTaxJunctionBuilder.build(103L, 102L));
+            // Coffee with Drink Tax
+            productTaxJunctionEntities.add(ProductTaxJunctionBuilder.build(104, 103));
+
+            // Insert entities into database instance.
+            DatabaseHelper.getInstance().getDatabase().getProductDao()
+                    .insertProducts(
+                            productEntities.toArray(new ProductEntity[0]));
+
+            DatabaseHelper.getInstance().getDatabase().getTaxDao().insertTaxes(
+                    taxEntities.toArray(new TaxEntity[0]));
+
+            DatabaseHelper.getInstance().getDatabase().getProductTaxJunctionDao().insertProductTaxJunctions(
+                    productTaxJunctionEntities.toArray(new ProductTaxJunctionEntity[0]));
+
+        }
+    }
+
+    private class OrderHistoryWorker implements Runnable {
+
+        @Override
+        public void run() {
+            List<CartEntity> cartEntities = new ArrayList<>();
+
+            List<CartProductEntity> cartProductEntities = new ArrayList<>();
+
+            cartEntities.add(CartBuilder.build(101L,new Date(),1000L,200L,1200L));
+
+            cartProductEntities.add(CartProductBuilder.build(101L,101L,"First Cart",1200L,1));
+
+            DatabaseHelper.getInstance().getDatabase().getCartDao().insertCarts(
+                    cartEntities.toArray(new CartEntity[0]));
+
+            DatabaseHelper.getInstance().getDatabase().getCartProductDao().insertCartProducts(
+                    cartProductEntities.toArray(new CartProductEntity[0]));
+
         }
     }
 }
